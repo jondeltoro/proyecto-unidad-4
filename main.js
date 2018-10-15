10c93488bfe867876c6320ec741e8308f9dfcb22
jonathan del toro <admin@jonathandeltoro.net>
2018-10-15 00:39:53 -0700

    update

--- main.js
+++ main.js
@@ -8,33 +8,7 @@ var config = {
   messagingSenderId: "734662032446"
 };
 firebase.initializeApp(config);
-$(document).ready(function () {
-  const map = L.map('map').setView([30, 0], 3);
-  map.scrollWheelZoom.disable();
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
-    maxZoom: 18
-  }).addTo(map);
-
-  const client = new carto.Client({
-    apiKey: '90629c29fd018b33d1cafc3839703d37d79b663e',
-    username: 'jondeltoro'
-  });
-
-  const source = new carto.source.Dataset('earthquake');
-  const style = new carto.style.CartoCSS(`
-        #layer {
-          marker-width: 7;
-          marker-fill: #EE4D5A;
-          marker-line-color: #FFFFFF;
-        }
-      `);
-  const layer = new carto.layer.Layer(source, style);
-
-  client.addLayer(layer);
-  client.getLeafletLayer().addTo(map);
-
-});
 
 
 var keys = [
